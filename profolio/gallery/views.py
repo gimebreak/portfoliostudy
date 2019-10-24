c54d3000b79a39b69599a82086744520508b105b
zhangzefly <344859306@qq.com>
2019-10-24 22:37:24 +0800

    新建gallery apps

---
+++ profolio/gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from gallery.models import Gallery
+
+def home(request):
+    gallerys = Gallery.objects.all()
+    return render(request,'gallery/home.html',{'gallerys':gallerys})
